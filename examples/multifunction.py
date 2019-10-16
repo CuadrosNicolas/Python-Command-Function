85c4c465c40ca318a7dc93e68882afb43b2824f8
Nicolas CUADROS <nicolas.cuadros@etu.univ-nantes.fr>
2019-10-16 15:53:29 +0200

    Adding cmdfusion examples.

---
+++ examples/multifunction.py
@@ -0,0 +1,25 @@
+from cmdfunc import cmdfusion
+
+cmd = cmdfusion("Test with multiple function.")
+@cmd.add
+def plus(a:int,b:int)->int:
+	"""
+	plus operation
+	:param a: first number
+	:param b: second number
+	:return: a+b
+	"""
+	return a+b
+@cmd.add
+def minus(a:int,b:int)->int:
+	"""
+	minus operation
+	:param a: first number
+	:param b: second number
+	:return: a-b
+	"""
+	return a-b
+
+out = cmd.main(__name__)
+if(out != None):
+	print(out)
