2e43a3184981bcad91a0cd2fa84b517331a7f567
Nicolas CUADROS <nicolas.cuadros@etu.univ-nantes.fr>
2019-10-16 15:53:17 +0200

    Adding cmdfusion to merge multiple function.

--- cmdfunc/__init__.py
+++ cmdfunc/__init__.py
@@ -3,3 +3,5 @@ from .analysis import UnsupportedTypeException
 from .analysis import WrongInputTypeException
 from .analysis import NonHomogenousEnumTypeException
 from .analysis import EnumHasNoTypeException
+from .analysis import IsNotAFunctionException
+from .cmdclass import cmdfusion
